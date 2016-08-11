4a13ea2297ed63d8e32cc092f369eec2042b9b91
jockeywu <wyxmail@163.com>
2016-08-11 23:14:07 +0800

    目前数据图表的状态先提交一下

--- src/com/sf/cup2/FragmentData.java
+++ src/com/sf/cup2/FragmentData.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend.LegendPosition;
+import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.XAxis.XAxisPosition;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.Legend.LegendForm;
@@ -17,6 +19,7 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import android.app.Fragment;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,12 +44,9 @@ public class FragmentData extends Fragment {
         mChart.setDescription("");
         mChart.setNoDataTextDescription("You need to provide data for the chart.");
     	
-        LineData mLineData = LineData(30);
-                 // 将x轴放到底部 默认在顶部
-                 XAxis mXAxis = mChart.getXAxis();
-                 mXAxis.setPosition(XAxisPosition.BOTTOM);
-         
-                 setChartStyle(mChart, mLineData);        
+        LineData mLineData = LineData(24);
+
+        setChartStyle(mChart, mLineData);        
         
         
         return view;
@@ -54,27 +54,45 @@ public class FragmentData extends Fragment {
  
     // 设置显示样式
       private void setChartStyle(LineChart mlinechart, LineData mLineData) {
+    	  
+          // 将x轴放到底部 默认在顶部
+          XAxis mXAxis = mlinechart.getXAxis();
+          mXAxis.setPosition(XAxisPosition.TOP);
+          LimitLine ll = new LimitLine(35f, "警戒线");  
+          ll.setLineColor(Color.RED);  
+          ll.setLineWidth(1f);  
+          ll.enableDashedLine(10f, 5f, 0f);
+          ll.setTextColor(Color.GRAY);  
+          ll.setTextSize(12f);  
+          // .. and more styling options  
+          YAxis leftAxis = mlinechart.getAxisLeft();
+          YAxis rightAxis = mlinechart.getAxisRight();
+          
+          rightAxis.setEnabled(false);
+          leftAxis.addLimitLine(ll);  	  
+    	  
+ 
           // 是否在折线上添加边框
           mlinechart.setDrawBorders(false);
           // 数据描述
-          mlinechart.setDescription("温度记录数据");
+          //mlinechart.setDescription("温度记录数据");
           // 如果没有数据的时候，会显示这个，类似listview的emtpyview
-          mlinechart.setNoDataTextDescription("如果传给MPAndroidChart的数据为空，那么你将看到这段文字。");
+          mlinechart.setNoDataTextDescription("喝水量异常,请检查与水杯蓝牙连接");
           // 是否绘制背景颜色。
           // 如果mLineChart.setDrawGridBackground(false)，
           // 那么mLineChart.setGridBackgroundColor()将失效;
-          mlinechart.setDrawGridBackground(true);
+          mlinechart.setDrawGridBackground(false);
           // 折线图的背景
           mlinechart.setGridBackgroundColor(Color.CYAN);
           // 设置触摸
       mlinechart.setTouchEnabled(true);
        // 设置拖拽
-          mlinechart.setDragEnabled(true);
+          mlinechart.setDragEnabled(false);
           // 设置缩放
-         mlinechart.setScaleEnabled(true);
+         mlinechart.setScaleEnabled(false);
           mlinechart.setPinchZoom(false);
           // x y 轴的背景
-          mlinechart.setBackgroundColor(Color.YELLOW);
+       //   mlinechart.setBackgroundColor(Color.YELLOW);
           // 设置x y轴的数据
           mlinechart.setData(mLineData);
           // 设置比例图标，就是那一组y的value的
@@ -87,25 +105,29 @@ public class FragmentData extends Fragment {
           // 下方字体颜色
           mLegend.setTextColor(Color.BLUE);
           // 设置x轴的动画
-          mlinechart.animateX(15000);
+          mlinechart.animateX(1000);
   
       }
   
       private LineData LineData(int count) {
           ArrayList<String> x = new ArrayList<String>();
           // x轴的数据
+
           for (int i = 0; i < count; i++) {
-              x.add("第" + i + "天");
+              String times = getString(R.string.times);
+              times = String.format(times, i);
+        	  
+              x.add(times);
           }
           // y轴的数据
           ArrayList<Entry> y = new ArrayList<Entry>();
           for (int i = 0; i < count; i++) {
-              float f = (float) (Math.random() * 100);
+              float f = (float) (Math.random() * 50);
               Entry entry = new Entry(f, i);
               y.add(entry);
           }
           // y轴的数据集
-          LineDataSet set = new LineDataSet(y, "数据集 --木子");
+          LineDataSet set = new LineDataSet(y, "");
           // 用y轴的集合来设置参数
           // 线宽
           set.setLineWidth(3.0f);
@@ -135,12 +157,13 @@ public class FragmentData extends Fragment {
          // 填充曲线下方的区域，红色，半透明。
          set.setDrawFilled(true);
         // 数值越小 透明度越大
-        set.setFillAlpha(150);
+        set.setFillAlpha(50);
         set.setFillColor(Color.RED);
  
          // 填充折线上数据点、圆球里面包裹的中心空白处的颜色。
-         set.setCircleColorHole(Color.YELLOW);
+      //   set.setCircleColorHole(Color.YELLOW);
          // 设置折线上显示数据的格式。如果不设置，将默认显示float数据格式。
+   
          set.setValueFormatter(new ValueFormatter() {
  
              @Override
@@ -148,9 +171,11 @@ public class FragmentData extends Fragment {
                      ViewPortHandler viewPortHandler) {
                  int n = (int) value;
                  String str = n + "℃";
-                 return str;
+               //  return str;
+                 return "";
              }
          });
+  
          ArrayList<LineDataSet> mLineDataSets = new ArrayList<LineDataSet>();
          mLineDataSets.add(set);
  
