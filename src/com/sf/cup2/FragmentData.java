859654fab1f0d45a519ff7a4124f46d24ccc7762
Administrator <Administrator@192.168.1.127>
2016-09-24 17:02:34 +0800

    微信,微信朋友圈分享功能实现

--- src/com/sf/cup2/FragmentData.java
+++ src/com/sf/cup2/FragmentData.java
@@ -6,8 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.wechat.friends.Wechat.ShareParams;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend.LegendPosition;
@@ -220,6 +222,16 @@ public class FragmentData extends Fragment {
 	
 	private void showShare() {
 		 ShareSDK.initSDK(getActivity());
+		 
+//		 
+//		 ShareParams wechat = new ShareParams();
+//         wechat.setTitle("我是分享标题");
+//         wechat.setText("我是分享文本内容");
+//         wechat.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+// 
+//         wechat.setUrl("http://mob.com");
+//         wechat.setShareType(Platform.SHARE_WEBPAGE);
+//		 
 		 OnekeyShare oks = new OnekeyShare();
 		 //关闭sso授权
 		 oks.disableSSOWhenAuthorize(); 
@@ -234,16 +246,18 @@ public class FragmentData extends Fragment {
 		 oks.setText("我是分享文本");
 		 //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
 		 //	 oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+		 	oks.setImageUrl("http://img.redocn.com/sheji/20160728/daimengkatongxiaonvhai_6799783_small.jpg"); 
+		 	
 		 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 		 //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
 		 // url仅在微信（包括好友和朋友圈）中使用
-		 oks.setUrl("http://sharesdk.cn");
+	//	 oks.setUrl("http://sharesdk.cn");
 		 // comment是我对这条分享的评论，仅在人人网和QQ空间使用
 		 oks.setComment("我是测试评论文本");
 		 // site是分享此内容的网站名称，仅在QQ空间使用
 		 oks.setSite(getString(R.string.app_name));
 		 // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-		 oks.setSiteUrl("http://sharesdk.cn");
+	//	 oks.setSiteUrl("http://sharesdk.cn");
 
 		// 启动分享GUI
 		 oks.show(getActivity());
